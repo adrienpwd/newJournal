6355e3b81789b67da6c7c804f4900fcab4154469
Adrien <adrien.poirson@gmail.com>
2020-07-09 10:27:23 -0600

    DEV config

--- client/src/actions/trades.js
+++ client/src/actions/trades.js
@@ -2,7 +2,13 @@ import axios from 'axios'
 
 // process.env.REACT_APP_USERS_SERVICE_URL has to be set to "http://localhost:5001"
 // Need to be do (root of the project)
+
+// For DEV env
 // export REACT_APP_USERS_SERVICE_URL=http://localhost:5001
+
+// For PROD env
+// export REACT_APP_USERS_SERVICE_URL=http://192.168.1.70:5001
+// Make sure the IP of the server (Intel NUC computer) didn't change
 
 export function loadTrades(start, end) {
   return (dispatch) => {
