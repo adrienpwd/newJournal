cb8d373bb3d8b11f111473ab1967107681f9ca58
Adrien <adrien.poirson@gmail.com>
2021-02-27 11:34:26 -0700

    Add Collapse to Description and Fix Seed Time

--- client/src/components/SeedCard/index.js
+++ client/src/components/SeedCard/index.js
@@ -1,30 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { strategies } from '../../utils';
-import { useDrop } from 'react-dnd';
-import { Link, useRouteMatch } from 'react-router-dom';
-import { CaretDown32, CaretUp32, WarningAlt32 } from '@carbon/icons-react';
+import React, { useState, useEffect } from 'react'
+import { strategies } from '../../utils'
+import { useDrop } from 'react-dnd'
+import { Link, useRouteMatch } from 'react-router-dom'
+import { CaretDown32, CaretUp32, WarningAlt32 } from '@carbon/icons-react'
 
-import styles from './seedCard.module.css';
+import styles from './seedCard.module.css'
 
-export default props => {
-  const { seed, unlinked } = props;
-  const match = useRouteMatch();
+export default (props) => {
+  const { seed, unlinked } = props
+  const match = useRouteMatch()
 
-  const getTradeType = isLong => (isLong ? <CaretUp32 /> : <CaretDown32 />);
+  const getTradeType = (isLong) => (isLong ? <CaretUp32 /> : <CaretDown32 />)
 
   const renderStrategy = () => {
-    const strategy = strategies.find(s => seed.strategy === s.id);
+    const strategy = strategies.find((s) => seed.strategy === s.id)
 
-    const strategyClass = strategy?.label ? null : styles.strategyMissing;
-    return (
-      <div className={strategyClass}>
-        Strategie: {strategy?.label || 'Strategy Missing'}
-      </div>
-    );
-  };
-
-  function createMarkup() {
-    return { __html: seed?.description };
+    const strategyClass = strategy?.label ? null : styles.strategyMissing
+    return <div className={strategyClass}>Strategie: {strategy?.label || 'Strategy Missing'}</div>
   }
 
   const renderSeedCard = () => {
@@ -34,9 +26,9 @@ export default props => {
           <WarningAlt32 className={styles.unlinkedIcon} />
           <span>Unlinked</span>
         </div>
-      );
+      )
     } else {
-      const arrowClass = seed.isLong ? styles.arrowLong : styles.arrowShort;
+      const arrowClass = seed.isLong ? styles.arrowLong : styles.arrowShort
       return (
         <div>
           <Link to={`/review/${match.params.day}/${seed.id}`}>
@@ -51,32 +43,28 @@ export default props => {
           <div className={styles.element}>{renderStrategy()}</div>
           <div>Price: {`$${seed.price}`}</div>
         </div>
-      );
+      )
     }
-  };
+  }
 
   const [{ canDrop, isOver }, drop] = useDrop({
     accept: 'Box',
     drop: () => ({ name: seed?.id || 'unlink' }),
-    collect: monitor => ({
+    collect: (monitor) => ({
       isOver: monitor.isOver(),
       canDrop: monitor.canDrop()
     })
-  });
-  const isActive = canDrop && isOver;
-  const borderColor = unlinked ? '#d7ccc8' : '#a1887f';
-  let backgroundColor = '#f5f5f6';
+  })
+  const isActive = canDrop && isOver
+  const borderColor = unlinked ? '#d7ccc8' : '#a1887f'
+  let backgroundColor = '#f5f5f6'
   if (isActive) {
-    backgroundColor = '#dcdcdd';
+    backgroundColor = '#dcdcdd'
   }
 
   return (
-    <div
-      ref={drop}
-      style={{ backgroundColor, borderColor }}
-      className={styles.container}
-    >
+    <div ref={drop} style={{ backgroundColor, borderColor }} className={styles.container}>
       <div className={styles.card}>{renderSeedCard()}</div>
     </div>
-  );
-};
+  )
+}
