f7adb395c02465cfb3463aff4d5e6fd817cd308b
Adrien Poirson <adrien.poirson@calabrio.com>
2021-04-25 16:43:59 -0600

    Add auto commissions

--- app/app.py
+++ app/app.py
@@ -306,12 +306,12 @@ def consolidate_trade(all_trades, built_trades, orders_dictionary):
             nb_shares = get_nb_shares(trade_actions)
             initial_trade['nb_shares'] = nb_shares
 
-            # if initial_trade['account'] == 'TRPCT0094':
-                # commissions = 0.005 * nb_shares
-                # initial_trade['commissions'] = 0.005 * nb_shares
-                # initial_trade['ratio_com_gain'] = round(
-                    # abs(commissions / gross_gain), 4)
-                # initial_trade['net_gain'] = round(gross_gain - commissions, 4)
+            commissions = 0.0035 * nb_shares
+            if commissions < 0.35:
+              commissions = 0.35
+            initial_trade['commissions'] = 0.005 * nb_shares
+            initial_trade['ratio_com_gain'] = round(abs(commissions / gross_gain), 4)
+            initial_trade['net_gain'] = round(gross_gain - commissions, 4)
 
             # slippage
             initial_trade['slippage'] = get_slippage(order_actions)
